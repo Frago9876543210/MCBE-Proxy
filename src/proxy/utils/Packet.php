24d0bddb7c930cdef38d3e800152c8f72426c801
alex <al.volf.av@gmail.com>
2018-02-24 19:43:54 +0300

    Support splits

--- src/proxy/utils/Packet.php
+++ src/proxy/utils/Packet.php
@@ -12,30 +12,79 @@ use proxy\hosts\BaseHost;
 use raklib\protocol\{
 	Datagram, EncapsulatedPacket
 };
+use raklib\server\Session;
 
 class Packet{
 	/** @var int $number */
 	public static $number = 0;
+	/** @var Datagram[][] */
+	private static $splitPackets = [];
 
 	/**
 	 * @param string $buffer
 	 * @return null|DataPacket
 	 */
 	public static function readDataPacket(string $buffer) : ?DataPacket{
-		if($buffer{0} === "\x84"){
-			$dataPacket = new Datagram;
-			$dataPacket->buffer = $buffer;
-			$dataPacket->decode();
-			self::$number = $dataPacket->seqNumber;
-			foreach($dataPacket->packets as $encapsulatedPacket){
-				/** @var BatchPacket $batch */
-				if(($batch = PacketPool::getPacket($encapsulatedPacket->buffer)) instanceof BatchPacket){
-					@$batch->decode();
-					if($batch->payload !== "" && is_string($batch->payload)){
-						foreach($batch->getPackets() as $buf){
-							return PacketPool::getPacket($buf);
+		$pid = ord($buffer{0});
+		if(($pid & Datagram::BITFLAG_VALID) !== 0){
+			if($pid & Datagram::BITFLAG_ACK){
+				//todo
+			}elseif($pid & Datagram::BITFLAG_NAK){
+				//todo
+			}else{
+				if(($datagram = new Datagram($buffer)) instanceof Datagram){
+					$datagram->decode();
+					self::$number = $datagram->seqNumber; //todo: check this
+					foreach($datagram->packets as $packet){
+						if($packet->hasSplit){
+							$split = self::decodeSplit($packet);
+							if($split !== null){
+								$packet = $split;
+							}
 						}
+						return self::decodeBatch($packet);
 					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static function decodeSplit(EncapsulatedPacket $packet) : ?EncapsulatedPacket{
+		if($packet->splitCount >= Session::MAX_SPLIT_SIZE or $packet->splitIndex >= Session::MAX_SPLIT_SIZE or $packet->splitIndex < 0){
+			return null;
+		}
+
+		if(!isset(self::$splitPackets[$packet->splitID])){
+			if(count(self::$splitPackets) >= Session::MAX_SPLIT_COUNT){
+				return null;
+			}
+			self::$splitPackets[$packet->splitID] = [$packet->splitIndex => $packet];
+		}else{
+			self::$splitPackets[$packet->splitID][$packet->splitIndex] = $packet;
+		}
+
+		if(count(self::$splitPackets[$packet->splitID]) === $packet->splitCount){
+			$pk = new EncapsulatedPacket;
+			$pk->buffer = "";
+			for($i = 0; $i < $packet->splitCount; ++$i){
+				$pk->buffer .= self::$splitPackets[$packet->splitID][$i]->buffer;
+			}
+
+			$pk->length = strlen($pk->buffer);
+			unset(self::$splitPackets[$packet->splitID]);
+			return $pk;
+		}
+		return null;
+	}
+
+	public static function decodeBatch(EncapsulatedPacket $encapsulatedPacket) : ?DataPacket{
+		/** @var $batch BatchPacket */
+		if(($batch = PacketPool::getPacket($encapsulatedPacket->buffer)) instanceof BatchPacket){
+			@$batch->decode();
+			if($batch->payload !== "" && is_string($batch->payload)){
+				foreach($batch->getPackets() as $buf){
+					return PacketPool::getPacket($buf);
 				}
 			}
 		}
